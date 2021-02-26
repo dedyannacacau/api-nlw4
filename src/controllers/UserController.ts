a722d65ac73bef2a7629525c3057f4b66a983a3d
Dedyanna Cacau <dedyannacacau@gmail.com>
2021-02-26 17:18:15 -0300

    dia 3 - testes

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,14 +1,16 @@
-import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { json, Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/UserRepository';
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
 
-    const usersRepository = getRepository(User);
+    const usersRepository = getCustomRepository(UserRepository);
 
-    const userAlreadyExists = usersRepository.findOne(email);
+    const userAlreadyExists = await usersRepository.findOne({
+      email,
+    });
 
     if (userAlreadyExists) {
       return response.status(400).json({ error: 'User already exists' });
@@ -21,7 +23,14 @@ class UserController {
 
     await usersRepository.save(user);
 
-    return response.json(user);
+    return response.status(201).json(user);
+  }
+  async show(request: Request, response: Response) {
+    const usersRepository = getCustomRepository(UserRepository);
+
+    const all = await usersRepository.find();
+
+    return response.json(all);
   }
 }
 
