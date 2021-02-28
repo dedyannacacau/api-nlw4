ca59a798d16ac091480ae7287d6b8e898e7db884
Dedyanna Cacau <dedyannacacau@gmail.com>
2021-02-28 18:01:49 -0300

    dia 5 - finalizando api com validações

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app';
 
 app.listen(3333, () => {
-  console.log('🔥 Server started at http://localhost:3333');
+  console.log('🔥 Server started at http://localhost:3333 🔥 ');
 });
